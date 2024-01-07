94a5342b4c9466bae24ed77e7402fe7dfc071fa7
YounCheEun <amy4021@naver.com>
2024-01-08 08:59:16 +0900

    initial commit

---
+++ src/main/java/TaskWeaver/TaskWeaver/TaskWeaverApplication.java
@@ -0,0 +1,13 @@
+package TaskWeaver.TaskWeaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskWeaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TaskWeaver/TaskWeaver/TaskWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskWeaver.TaskWeaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskWeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

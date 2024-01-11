81c7aadc7a7598a4165a3cb30a03e724663b4c33
minji Kim <101926082+bboggo@users.noreply.github.com>
2024-01-11 15:22:08 +0900

    Update TaskWeaverApplication.java

--- src/main/java/TaskWeaver/TaskWeaver/TaskWeaverApplication.java
+++ src/main/java/TaskWeaver/TaskWeaver/TaskWeaverApplication.java
@@ -2,10 +2,8 @@ package TaskWeaver.TaskWeaver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-
+@SpringBootApplication
 public class TaskWeaverApplication {
 
 	public static void main(String[] args) {

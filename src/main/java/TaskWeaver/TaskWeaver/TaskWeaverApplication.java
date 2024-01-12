d13666663a65d8121c88c13d4b254b2cbd24e663
bboggo <kmt0819@naver.com>
2024-01-13 00:11:32 +0900

    Update jar

--- src/main/java/TaskWeaver/TaskWeaver/TaskWeaverApplication.java
+++ src/main/java/TaskWeaver/TaskWeaver/TaskWeaverApplication.java
@@ -2,12 +2,19 @@ package TaskWeaver.TaskWeaver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TaskWeaverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskWeaverApplication.class, args);
+	}
+	@GetMapping(value = "/")
+	public String helloWorld() {
+		return "CI/CD PLEASE";
 	}
 
 }
